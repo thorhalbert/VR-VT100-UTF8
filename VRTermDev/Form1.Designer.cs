ae87d92dafbb42cd1f26ff0c0f96739085308563
thorhalbert <thorhalbert@gmail.com>
2018-11-20 13:07:28 -0800

    Change solution name and switch to Nuget Nunit.

---
+++ VRTermDev/Form1.Designer.cs
@@ -0,0 +1,257 @@
+﻿namespace VRTermDev
+{
+    partial class VRTermMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.host_textbox = new System.Windows.Forms.TextBox();
+            this.user_textbox = new System.Windows.Forms.TextBox();
+            this.pass_textbox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.disconnectButton = new System.Windows.Forms.Button();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.terminalFrameBuffer = new System.Windows.Forms.PictureBox();
+            this.screenInfo = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.flowLayoutPanel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.terminalFrameBuffer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.tableLayoutPanel1);
+            this.flowLayoutPanel1.Controls.Add(this.terminalFrameBuffer);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(1019, 900);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel2, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel3, 1, 0);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(981, 100);
+            this.tableLayoutPanel1.TabIndex = 7;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.label1);
+            this.flowLayoutPanel2.Controls.Add(this.host_textbox);
+            this.flowLayoutPanel2.Controls.Add(this.label2);
+            this.flowLayoutPanel2.Controls.Add(this.user_textbox);
+            this.flowLayoutPanel2.Controls.Add(this.label3);
+            this.flowLayoutPanel2.Controls.Add(this.pass_textbox);
+            this.flowLayoutPanel2.Controls.Add(this.screenInfo);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(3, 3);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(484, 94);
+            this.flowLayoutPanel2.TabIndex = 0;
+            // 
+            // host_textbox
+            // 
+            this.flowLayoutPanel2.SetFlowBreak(this.host_textbox, true);
+            this.host_textbox.Location = new System.Drawing.Point(56, 3);
+            this.host_textbox.Name = "host_textbox";
+            this.host_textbox.Size = new System.Drawing.Size(303, 20);
+            this.host_textbox.TabIndex = 1;
+            this.host_textbox.TextChanged += new System.EventHandler(this.host_textbox_TextChanged);
+            // 
+            // user_textbox
+            // 
+            this.flowLayoutPanel2.SetFlowBreak(this.user_textbox, true);
+            this.user_textbox.Location = new System.Drawing.Point(56, 29);
+            this.user_textbox.Name = "user_textbox";
+            this.user_textbox.Size = new System.Drawing.Size(303, 20);
+            this.user_textbox.TabIndex = 3;
+            this.user_textbox.TextChanged += new System.EventHandler(this.user_textbox_TextChanged);
+            // 
+            // pass_textbox
+            // 
+            this.flowLayoutPanel2.SetFlowBreak(this.pass_textbox, true);
+            this.pass_textbox.Location = new System.Drawing.Point(57, 55);
+            this.pass_textbox.Name = "pass_textbox";
+            this.pass_textbox.Size = new System.Drawing.Size(303, 20);
+            this.pass_textbox.TabIndex = 5;
+            this.pass_textbox.TextChanged += new System.EventHandler(this.pass_textbox_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(3, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Host:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(3, 26);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "User:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(3, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 20);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Pass:";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.connectButton);
+            this.flowLayoutPanel3.Controls.Add(this.disconnectButton);
+            this.flowLayoutPanel3.Controls.Add(this.statusLabel);
+            this.flowLayoutPanel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(493, 3);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(485, 94);
+            this.flowLayoutPanel3.TabIndex = 1;
+            // 
+            // connectButton
+            // 
+            this.connectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.connectButton.Location = new System.Drawing.Point(3, 3);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(97, 32);
+            this.connectButton.TabIndex = 8;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // disconnectButton
+            // 
+            this.disconnectButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.disconnectButton.Location = new System.Drawing.Point(106, 3);
+            this.disconnectButton.Name = "disconnectButton";
+            this.disconnectButton.Size = new System.Drawing.Size(97, 32);
+            this.disconnectButton.TabIndex = 9;
+            this.disconnectButton.Text = "Disconnect";
+            this.disconnectButton.UseVisualStyleBackColor = true;
+            this.disconnectButton.Click += new System.EventHandler(this.disconnectButton_Click);
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.statusLabel.AutoSize = true;
+            this.flowLayoutPanel3.SetFlowBreak(this.statusLabel, true);
+            this.statusLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(209, 0);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(43, 16);
+            this.statusLabel.TabIndex = 10;
+            this.statusLabel.Text = "status";
+            // 
+            // terminalFrameBuffer
+            // 
+            this.terminalFrameBuffer.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.terminalFrameBuffer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.terminalFrameBuffer.Location = new System.Drawing.Point(3, 109);
+            this.terminalFrameBuffer.Name = "terminalFrameBuffer";
+            this.terminalFrameBuffer.Size = new System.Drawing.Size(981, 767);
+            this.terminalFrameBuffer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.terminalFrameBuffer.TabIndex = 6;
+            this.terminalFrameBuffer.TabStop = false;
+            this.terminalFrameBuffer.SizeChanged += new System.EventHandler(this.terminalFrameBuffer_SizeChanged);
+            // 
+            // screenInfo
+            // 
+            this.screenInfo.AutoSize = true;
+            this.screenInfo.Location = new System.Drawing.Point(3, 78);
+            this.screenInfo.Name = "screenInfo";
+            this.screenInfo.Size = new System.Drawing.Size(57, 13);
+            this.screenInfo.TabIndex = 6;
+            this.screenInfo.Text = "screenInfo";
+            // 
+            // VRTermMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1019, 900);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "VRTermMain";
+            this.Text = "VR Terminal Development Sandbox";
+            this.SizeChanged += new System.EventHandler(this.VRTermMain_SizeChanged);
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.flowLayoutPanel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.terminalFrameBuffer)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox host_textbox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox user_textbox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pass_textbox;
+        private System.Windows.Forms.PictureBox terminalFrameBuffer;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.Button disconnectButton;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label screenInfo;
+    }
+}
+
---
+++ VRTermDev/VRTermMainWin.cs
@@ -0,0 +1,179 @@
+﻿using libVT100;
+using Renci.SshNet;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VRTermDev
+{
+    public partial class VRTermMain : Form
+    {
+        string fontName = "Consolas";
+        int fontSize = 12;
+        private Font screenFont;
+
+        int charWidth;
+        int charHeight;
+
+        int termWidth = 0;
+        int termHeight = 0;
+        private Bitmap terminalFrameBitmap;
+        private Graphics terminalGraphicsContext;
+
+        // Connection (SSH)
+        private SshClient client;
+        private IAnsiDecoder vt100;
+        private libVT100.Screen screen;
+
+        public VRTermMain()
+        {
+            InitializeComponent();
+
+            screenFont = new Font(fontName, fontSize);
+
+            VRTermMain_SizeChanged(this, new EventArgs());  // After this, we'll have a bitmap
+
+            checkLoginState();
+        }
+
+        private void checkLoginState()
+        {
+            if (client!=null && client.IsConnected)
+            {
+                connectButton.Enabled = false;
+                disconnectButton.Enabled = true;
+
+                statusLabel.Text = client.ConnectionInfo.Username + "@" + client.ConnectionInfo.Host + " [" + client.ConnectionInfo.ServerVersion+"]";
+
+                return;
+            }
+
+            bool allGood = true;
+            if (String.IsNullOrWhiteSpace(host_textbox.Text)) allGood = false;
+            if (String.IsNullOrWhiteSpace(user_textbox.Text)) allGood = false;
+            if (String.IsNullOrWhiteSpace(pass_textbox.Text)) allGood = false;
+
+            disconnectButton.Enabled = false;
+
+                connectButton.Enabled = allGood;
+
+            statusLabel.Text = "disconnected";
+        }
+
+        // With flow layout's its hard to get the picturebox to autosize - some hackery here
+        private void VRTermMain_SizeChanged(object sender, EventArgs e)
+        {
+            var left = terminalFrameBuffer.Left;
+            var top = terminalFrameBuffer.Top;
+
+            var fSize = this.Size;
+
+            terminalFrameBuffer.Size = new Size(fSize.Width - 25, fSize.Height - top - 50);  // hacky offsets
+        }
+
+        // Recompute the terminal size - this will cause our initial bitmap to be built
+        private void terminalFrameBuffer_SizeChanged(object sender, EventArgs e)
+        {
+            var tSize = terminalFrameBuffer.Size;
+
+            charWidth = screenFont.Height;    // HACK HACK - figure out how big an M is - this are fixed fonts
+            charHeight = screenFont.Height;
+
+            termWidth = tSize.Width / charWidth;
+            termHeight = tSize.Height / charHeight;
+
+            screenInfo.Text = termHeight + " x " + termWidth;
+
+            resizeTerminal();
+        }
+
+        private void resizeTerminal()
+        {
+            terminalFrameBitmap = new Bitmap(charWidth * termWidth, charHeight * termHeight);
+            terminalGraphicsContext = Graphics.FromImage(terminalFrameBitmap);
+
+            terminalFrameBuffer.Image = (Image)terminalFrameBitmap;
+        }
+
+        private void host_textbox_TextChanged(object sender, EventArgs e)
+        {
+            checkLoginState();
+        }
+
+        private void user_textbox_TextChanged(object sender, EventArgs e)
+        {
+            checkLoginState();
+        }
+
+        private void pass_textbox_TextChanged(object sender, EventArgs e)
+        {
+            checkLoginState();
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            if (client != null && client.IsConnected) return;
+
+            if (client != null)
+                client.Dispose();
+
+            client = new SshClient(host_textbox.Text, user_textbox.Text, pass_textbox.Text);
+
+            try
+            {
+                client.Connect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, "Connect: " + ex.Message, "Connect Error", MessageBoxButtons.OK);
+                client.Dispose();
+                client = null;
+
+                return;
+            }
+
+            checkLoginState();
+
+            var keyS = Console.OpenStandardInput();
+            var screenS = new ScreenStream();
+
+            vt100 = new AnsiDecoder();
+            screen = new libVT100.Screen(termWidth, termHeight);
+
+            vt100.Encoding = Encoding.GetEncoding("utf8");
+            vt100.Subscribe(screen);
+
+            screenS.AnsiDecoder = vt100;
+
+            vt100.Output += Vt100_Output;
+
+            var shell = client.CreateShell(keyS, screenS, screenS);  // stdin, stdout, stderr
+
+            shell.Start();
+        }
+
+        private void Vt100_Output(IDecoder _decoder, byte[] _output)
+        {
+          
+        }
+
+        private void disconnectButton_Click(object sender, EventArgs e)
+        {
+            if (client != null && client.IsConnected)
+                client.Disconnect();
+
+            if (client != null)
+                client.Dispose();
+
+            client = null;
+
+            checkLoginState();
+        }
+    }
+}
